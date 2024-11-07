a4f347fc594a9382ffb6f082272c9d8b2e39170f
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2024-11-07 18:06:04 +0100

    refactor with generics

--- backend/urls.py
+++ backend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path('tasks', views.TaskList.as_view(), name='task_list'),
     path('users', views.UserList.as_view(), name='user_list'),
-    path('tasks/<int:task_id>', views.TaskDetail.as_view(), name='task_detail'),
+    path('tasks/<int:pk>', views.TaskDetail.as_view(), name='task_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/views.py
+++ backend/views.py
@@ -6,48 +6,28 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
+from rest_framework import generics
 # Create your views here.
 class Index(APIView):
     def get(self, request):
         return HttpResponse("Hello, world. You're at the backend index")
 
-class TaskList(APIView):
-    def get(self, request):
-        tasks = Task.objects.all()
-        task = TaskSerializer(tasks, many=True)
-        return Response(task.data)
-    def post(self, request):
-        task_serializer = TaskSerializer(data=request.data)
-        if task_serializer.is_valid():
-            task_serializer.save()
-            return Response(task_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(task_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
     
-class TaskDetail(APIView):
-    def get_object(self, task_id):
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def get_object(self, pk):
         try:
-            task = Task.objects.get(pk=task_id)
+            return Task.objects.get(pk=pk)
         except Task.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
     
-    def get(self, request, task_id):
-        task = self.get_object(task_id)
-        task_serializer = TaskSerializer(task)
-        return Response(task_serializer.data)
-    def delete(self, request, task_id):
-        task = self.get_object(task_id)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    def put(self, request, task_id):
-        task = self.get_object(task_id)
-        task_serializer = TaskSerializer(task, data=request.data)
-        if task_serializer.is_valid():
-            task_serializer.save()
-            return Response(task_serializer.data, status=status.HTTP_201_CREATED)
-        return Response(task_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    def patch(self, request, task_id):
-        task = self.get_object(task_id)
+    def patch(self, request, pk):
+        task = self.get_object(pk)
         task_serializer = TaskSerializer(task, data=request.data, partial=True)
         if task_serializer.is_valid():
             task_serializer.save()
